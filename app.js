4618c555ccaace965658506c6cb8e7c4d7f2110a
Cristian <cristianatehortua1998@gmail.com>
2020-06-01 21:01:55 -0500

    Primer Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//Requires
+var express = require('express')
+var mongoose = require('mongoose')
+
+//Inicializar variables
+var app = express();
+
+//Coneccion a mongoDb
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, resp) => {
+    if (err) throw err;
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online')
+})
+
+//Rutas
+app.get('/', (req, res, next) => {
+        res.status(200).json({
+            ok: true,
+            mensaje: 'Peticion realizada correctamente'
+        })
+    })
+    //Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000: \x1b[32m%s\x1b[0m', 'online')
+})
